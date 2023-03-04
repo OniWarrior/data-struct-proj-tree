fc12add98dec9da03d0602ec8f3689c53947a653
Stephen Aranda <aranda.stephen88@gmail.com>
2023-03-03 19:47:34 -0600

    defined insert method

--- tree.cpp
+++ tree.cpp
@@ -6,3 +6,20 @@ Desc  : Specification file for tree.h
 */
 
 #include "tree.h"
+
+void IntBinaryTree::insert(TreeNode *&nodePtr, TreeNode *&newNode)
+{
+
+    if (nodePtr == nullptr)
+    {
+        nodePtr = newNode; // Insert the node.
+    }
+    else if (newNode->m_value < nodePtr->m_value)
+    {
+        insert(nodePtr->left, newNode); // Search the left branch.
+    }
+    else
+    {
+        insert(nodePtr->right, newNode); // Search the right branch.
+    }
+}
