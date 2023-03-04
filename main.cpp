e3e15390e431bdd7067239d8dee8e814a537b89f
Stephen Aranda <aranda.stephen88@gmail.com>
2023-03-03 20:47:59 -0600

    created a tree

--- main.cpp
+++ main.cpp
@@ -3,3 +3,12 @@
   Desc  : Entry point to answer data structs homework questions for current homework.
   Date  : 3/1/23
 */
+
+#include "tree.cpp"
+
+int main()
+{
+
+  // create the binary tree.
+  IntBinaryTree tree;
+}
